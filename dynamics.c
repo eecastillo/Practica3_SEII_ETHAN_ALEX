a7b518099a906a0a8a5bead356a68dd540fa67e5
Ethandrake Castillo <ie714410@iteso.mx>
2020-11-04 20:29:31 -0700

    initial files added

---
+++ dynamics.c
@@ -0,0 +1,8 @@
+/*
+ * dynamics.c
+ *
+ *  Created on: Nov 4, 2020
+ *      Author: eduar
+ */
+
+
---
+++ dynamics.h
@@ -0,0 +1,13 @@
+/*
+ * dynamics.h
+ *
+ *  Created on: Nov 4, 2020
+ *      Author: eduar
+ */
+
+#ifndef DYNAMICS_H_
+#define DYNAMICS_H_
+
+
+
+#endif /* DYNAMICS_H_ */
