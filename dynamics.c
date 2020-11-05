8f2096226871b1b4750eb9383b39fd8f51b04473
Ethandrake Castillo <ie714410@iteso.mx>
2020-11-04 21:47:59 -0700

    functions definitions added

--- dynamics.c
+++ dynamics.c
@@ -4,5 +4,33 @@
  *  Created on: Nov 4, 2020
  *      Author: eduar
  */
+#include "dynamics.h"
 
+uint8_t dibujar5(uint8_t *hid_buffer)
+{
 
+}
+uint8_t mouseRight_and_click(uint8_t *hid_buffer)
+{
+
+}
+uint8_t mouseLeft_and_click(uint8_t *hid_buffer)
+{
+
+}
+uint8_t keyboard_open_paint(uint8_t *bufferKey)
+{
+
+}
+uint8_t keyboard_open_notes(uint8_t *bufferKey)
+{
+
+}
+uint8_t keyboard_write_message(uint8_t *bufferKey)
+{
+
+}
+uint8_t keyboard_copy_message(uint8_t *bufferKey)
+{
+
+}
--- dynamics.h
+++ dynamics.h
@@ -8,6 +8,15 @@
 #ifndef DYNAMICS_H_
 #define DYNAMICS_H_
 
+#define CLICK 0x01
 
+
+uint8_t dibujar5(uint8_t *hid_buffer);
+uint8_t mouseRight_and_click(uint8_t *hid_buffer);
+uint8_t mouseLeft_and_click(uint8_t *hid_buffer);
+uint8_t keyboard_open_paint(uint8_t *bufferKey);
+uint8_t keyboard_open_notes(uint8_t *bufferKey);
+uint8_t keyboard_write_message(uint8_t *bufferKey);
+uint8_t keyboard_copy_message(uint8_t *bufferKey);
 
 #endif /* DYNAMICS_H_ */
