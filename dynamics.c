88ddb1d5be18727522e11ff6a2001e85a3b70fba
Alejandro Gudiño <gudo0399@gmail.com>
2020-11-04 22:47:08 -0600

    open paint created

--- dynamics.c
+++ dynamics.c
@@ -5,4 +5,30 @@
  *      Author: eduar
  */
 
+#include "dynamics.h"
+
+
+uint8_t openPaint(uint8_t *bufferKey)
+{
+    static int x = 0;
+    static unit8_t flag = running;
+    static uint8_t dir = step1;
+    static uint8_t keys_array[] = {KEY_RIGTH_GUI, KEY_P, KEY_A, KEY_I, KEY_N, KEY_T, KEY_ENTER};
+
+    bufferKey[2] = 0;
+    if(flag == 0){
+        x++;
+        if ( x > 200){
+            bufferKey[2] = keys_array[dir];
+            dir ++;
+            x = 0;
+            if(dir == 8){
+                dir = step1;
+                flag = ready;
+                bufferKey[2] = 0;
+            }
+        }
+    }
+    return flag;
+}
 
